83fb676c415713a5fbf7765689f9ab25fd63b8b6
Ben <ben.a.burgos@gmail.com>
2022-05-24 20:40:58 -0700

    adds db connection

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
-// Dependencies
+// Dependencies //////////////////////////////////////////////////////////
 require('dotenv').config();
+const { PORT = 3001, DATABASE_URL } = process.env;
 const express = require('express');
+const mongoose = require('mongoose');
 
-// App Object
+// App Object ////////////////////////////////////////////////////////////
 const app = express();
 
-// Routes
+// Database Connection ///////////////////////////////////////////////////
+// Establish Connection
+mongoose.connect(DATABASE_URL);
+// Connection Events
+mongoose.connection
+  .on('open', () => console.log(`You are connected to MongoDB`))
+  .on('close', () => console.log(`You are disconnected from MongoDB`))
+  .on('error', (error) => console.log(error));
+
+// Routes ////////////////////////////////////////////////////////////////
 app.get('/', (req, res) => {
   res.send(`You're at the index!`);
 });
 
-// Listener
-const PORT = process.env.PORT || 3001;
+// Listener //////////////////////////////////////////////////////////////
 app.listen(PORT, () => console.log(`You're listening on port ${PORT}!`));
